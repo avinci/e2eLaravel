869d5cc7f91c6ef5d177d03901cab0eff35ab264
Avi Cavale <avi@shippable.com>
2018-09-04 12:43:09 -0700

    adding pg prov

---
+++ tf/rds.tf
@@ -0,0 +1,48 @@
+resource "aws_db_subnet_group" "pg_subnet_group" {
+  name = "pg_subnet_group"
+  subnet_ids = [
+    "${aws_subnet.vpc_public_sn.id}"]
+}
+
+resource "aws_db_instance" "pg_db" {
+  identifier = "pg"
+  allocated_storage = 10
+  storage_type = "gp2"
+  engine = "postgres"
+  engine_version = "9.6"
+  instance_class = "db.t2.micro"
+  parameter_group_name = "default.postgres9.6"
+  skip_final_snapshot = true
+  apply_immediately = true
+  db_subnet_group_name = "${aws_db_subnet_group.pg_subnet_group.name}"
+  vpc_security_group_ids = [
+    "${aws_security_group.vpc_public_sg.id}"]
+  multi_az = false
+  name = "${var.postgres_db_name}"
+  username = "${var.postgres_db_username}"
+  password = "${var.postgres_db_password}"
+}
+
+output "engine" {
+  value = "${aws_db_instance.pg_db.engine}"
+}
+
+output "engine_version" {
+  value = "${aws_db_instance.pg_db.engine_version}"
+}
+
+output "endpoint" {
+  value = "${aws_db_instance.pg_db.endpoint}"
+}
+
+output "port" {
+  value = "${aws_db_instance.pg_db.port}"
+}
+
+output "db_name" {
+  value = "${aws_db_instance.pg_db.name}"
+}
+
+output "db_user" {
+  value = "${aws_db_instance.pg_db.username}"
+}
